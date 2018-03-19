94a632e4720b170f730ec1340da9f4fb06b7535f
Pyo <pyo@robotis.com>
2018-03-19 16:33:24 +0900

    refactoring to release

--- turtlebot3_autorace_detect/setup.py
+++ turtlebot3_autorace_detect/setup.py
@@ -1,4 +1,5 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+## See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
