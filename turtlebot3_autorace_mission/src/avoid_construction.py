512193178b1a2a1d0c60aac53b0aea0edccfdab4
chan-1207 <dddoggi1207@gmail.com>
2025-02-19 16:00:07 +0900

    fix lint

--- turtlebot3_autorace_mission/src/avoid_construction.py
+++ turtlebot3_autorace_mission/src/avoid_construction.py
@@ -22,10 +22,10 @@ import math
 
 import cv2
 from cv_bridge import CvBridge
-import numpy as np
 from geometry_msgs.msg import Twist
-import rclpy
+import numpy as np
 from nav_msgs.msg import Odometry
+import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
